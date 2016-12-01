031407f43b8b62bb23bd38f633015207c0688636
Arnelle Balane <arnellebalane@gmail.com>
2016-12-01 16:27:44 +0800

    simplify and wrap clientside js inside iife

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,11 +1,13 @@
-const server = new EventSource('/eventsource');
-let token;
+(function() {
+    let token;
 
-server.addEventListener('open', e => {
-    console.info('EventSource connection to server opened');
-});
 
-server.addEventListener('subscribe', e => {
-    console.info('Subscribed to server-sent events');
-    token = e.data;
-});
+    const subscribeToServerSentEvents = url => {
+        const server = new EventSource(url);
+
+        server.addEventListener('subscribe', e => token = e.data);
+    };
+
+
+    subscribeToServerSentEvents('/eventsource');
+})();
