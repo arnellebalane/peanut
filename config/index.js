f629529618fe7d3bfa0459a8bd4a6e92a2d3df8c
Arnelle Balane <arnellebalane@gmail.com>
2017-05-12 11:06:47 +0800

    update some parts of the codebase to new conventions

--- config/index.js
+++ config/index.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const nconf = require('nconf');
 
-
 nconf
     .argv()
     .env()
-    .file({ file: path.join(__dirname, 'config.json') })
+    .file('secrets', path.join(__dirname, 'secrets.json'))
+    .file('config', path.join(__dirname, 'config.json'))
     .defaults({
         PORT: 3000
     });
-
 
 module.exports = nconf;
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const winston = require('winston');
 const consolidate = require('consolidate');
 const config = require('./config');
 
-
 const app = express();
 
 app.engine('html', consolidate.nunjucks);
@@ -15,4 +14,4 @@ app.use(morgan('dev'));
 app.use(require('./lib/routes'));
 
 app.listen(config.get('PORT'),
-    _ => winston.info(`Server is now running at port ${config.get('PORT')}`));
+    () => winston.info(`Server is now running at port ${config.get('PORT')}`));
