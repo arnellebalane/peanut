7aed6c9f804f712973e383f5d5b38ef61ae71add
Arnelle Balane <arnellebalane@gmail.com>
2017-05-30 15:10:41 +0800

    use websockets for signalling via socket.io

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 const morgan = require('morgan');
 const winston = require('winston');
 const consolidate = require('consolidate');
@@ -12,8 +14,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan('dev'));
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/socketio', express.static(
+    path.join(__dirname, 'node_modules', 'socket.io-client', 'dist')));
 
 app.get('/', (req, res) => res.render('index.html'));
 
-app.listen(config.get('PORT'),
+const server = http.createServer(app);
+server.listen(config.get('PORT'),
     () => winston.info(`Server is now running at port ${config.get('PORT')}`));
+
+const io = socketio(server);
+const clients = [];
+
+io.on('connection', (socket) => {
+    clients.push(socket);
+});
