7f64b456ea80bdfe16add3e0180044114c2ae422
Arnelle Balane <arnellebalane@gmail.com>
2017-05-30 14:17:13 +0800

    reset project files

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.engine('html', consolidate.nunjucks);
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan('dev'));
-app.use(require('./lib/routes'));
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => res.render('index.html'));
 
 app.listen(config.get('PORT'),
     () => winston.info(`Server is now running at port ${config.get('PORT')}`));
